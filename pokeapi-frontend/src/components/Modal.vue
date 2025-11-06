51e2dc32502d6a6e1dacce6b46f6dd60e1a44467
horbinrodas <horbinrodas@gmail.com>
2025-11-06 10:41:52 -0600

    updated styles

--- pokeapi-frontend/src/components/Modal.vue
+++ pokeapi-frontend/src/components/Modal.vue
@@ -1,6 +1,7 @@
 <template>
   <div v-if="showModal" class="modal-overlay" @click.self="closeModal">
     <div class="modal-content">
+      <img class="bg-pokedex" src="/pokedex.png" alt="Pokedex" />
       <button @click="closeModal">X</button>
       <h3>{{ selectedPokemon.name }}</h3>
       <div class="slider">
@@ -8,16 +9,45 @@
           <swiper-slide v-for="(img, index) in spriteImages" :key="index">
             <img :src="img" :alt="`Sprite ${index}`" />
           </swiper-slide>
+          <swiper-slide>
+            <ul>
+              <li>Altura: {{ selectedPokemon.height }}</li>
+              <li>Peso: {{ selectedPokemon.weight }}</li>
+              <li>Experiencia Base: {{ selectedPokemon.base_experience }}</li>
+              <li>Abilidades: {{ selectedPokemon.abilities.map(ability => ability.ability.name).join(', ') }}</li>
+              <li>Tipos: {{ selectedPokemon.types.map(type => type.type.name).join(', ') }}</li>
+              <li>Especie: {{ selectedPokemon.species.name }}</li>
+              <li v-for="stat in selectedPokemon.stats" :key="stat.stat.name">
+                {{ stat.stat.name }}: {{ stat.base_stat }}
+              </li>
+            </ul>
+            <audio controls>
+              <source :src="selectedPokemon.cries.latest" type="audio/mpeg">
+            </audio>
+          </swiper-slide>
+          <swiper-slide>
+            <div class="table-container">
+              <table>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Movimientos</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(m, i) in selectedPokemon.moves" :key="m.move.name + i">
+                    <td>{{ i + 1 }}</td>
+                    <td>{{ m.move.name }}</td>
+                  </tr>
+                  <tr v-if="!selectedPokemon.moves || !selectedPokemon.moves.length">
+                    <td>No hay movimientos</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </swiper-slide>
         </swiper>
       </div>
-      <ul>
-        <li>Altura: {{ selectedPokemon.height }}</li>
-        <li>Peso: {{ selectedPokemon.weight }}</li>
-        <li>Experiencia Base: {{ selectedPokemon.base_experience }}</li>
-        <li v-for="stat in selectedPokemon.stats" :key="stat.stat.name">
-          {{ stat.stat.name }}: {{ stat.base_stat }}
-        </li>
-      </ul>
     </div>
   </div>
 </template>
@@ -35,9 +65,13 @@
 
   const openModal = (pokemon) => {
     selectedPokemon.value = pokemon
-    spriteImages.value = Object.values(pokemon.sprites).filter(
-      (img) => typeof img === 'string' && img.includes('http')
-    )
+    const sprites = pokemon.sprites || {}
+    spriteImages.value = []
+    const official = sprites.other?.['official-artwork']?.front_default
+    if (official) spriteImages.value.push(official)
+    if (sprites.front_default) spriteImages.value.push(sprites.front_default)
+    if (sprites.back_default) spriteImages.value.push(sprites.back_default)
+
     showModal.value = true
   }
 
@@ -57,15 +91,31 @@
     display: flex; justify-content: center; align-items: center;
   }
   .modal-content {
+    position: relative;
     background: #FF0000;
-    padding: 1rem;
+    opacity: 0.75;
     border-radius: 8px;
     width: 80%;
-    max-width: 500px;
+    height: 100%;
+    max-width: 40%;
+    color: white;
+  }
+  @media screen and (max-width: 768px) {
+    .modal-content {
+      width: 90%;
+      max-width: 90%;
+    }
   }
   .slider img {
     width: 100%;
     height: auto;
+  }
+  .bg-pokedex {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    opacity: 0.75;
+    z-index: -1;
   }
   button {
     position: absolute;
@@ -74,5 +124,36 @@
     border: none;
     font-size: 1.5rem;
     cursor: pointer;
+  }
+  audio {
+    width: 80%;
+    padding-left: 3rem;
+  }
+  ul {
+    list-style-type: disc;
+    padding-left: 6rem;
+    padding-top: 6rem;
+    font-size: x-large;
+  }
+  h3 {
+    text-align: center;
+    margin-top: 1rem;
+  }
+  .table-container {
+    max-height: 240px; 
+    overflow-y: auto; 
+    padding: 6rem;
+    top: 5rem;
+    scrollbar-color: transparent transparent;
+  }
+  table{
+    width: 100%; 
+    border-collapse: collapse; 
+    color: white;
+  }
+  th, td {
+    border: 1px solid #ccc;
+    padding: 8px;
+    font-size: x-large;
   }
 </style>
